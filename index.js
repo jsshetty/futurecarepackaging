53096b5aa4ff17434108b54303fb20bc647a0343
sshetty <Sharatha_Shetty@intuit.com>
2019-08-11 17:02:03 +0530

    update

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 
-// var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ? true : false;
-
-// console.log(isMobile);
-
-// $(document).ready( function(){
-//     if (!isMobile) {
-//         $('.connectUs a').mouseover( function () { 
-//             $(".tool-tip").css("display", "block");
-//         })
-//         .mouseleave( function () { 
-//             $(".tool-tip").css("display", "none");
-//         });
-//     }
-// });
+$(document).ready( function(){
+    $('.connectUs a').click( function () { 
+        $(".tool-tip").css("display", "block");
+    })
+    $('.tool-tip button').click( function () { 
+        $(".tool-tip").css("display", "none");
+    })
+});
